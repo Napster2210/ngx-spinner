8a0802b4fd6417404b35e7933a689e076a4d0f37
alexAlencar <avieira@indracompany.com>
2018-10-22 22:52:19 -0300

    Showcase of all added features at ng6-spinner
    Remember when I said, back in cc32b4d7bd73f5, that I would make a showcase
    with all changes. So there you have it.
    
    We got multiple spinners, we got one full screen spinner that can have its
    properties changed by passing arguments to show() call(there's a button
    that does that, no need to worry to go to definition and implement
    yourself, though I highly suggest, for you can really understand how it
    works);
    
    And yeah, I think that's that.
    
    It generally helps in clarifying questions w.r.t spinner's implementation.
    
    As soon as this PR gets approved(I hope, it took me quite a while to
    implement all this), I'll personally rewrite the README.MD to match these
    new specifications.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,34 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class AppComponent {
 
+  loadingText: string;
+
   public spinnerConfig: any = {
-    bdColor: 'rgba(51,51,51,0.8)',
-    size: 'large',
-    color: '#fff',
-    loadigText: 'Loading...'
+    bdColor: 'rgba(55,55,51,0.8)',
+    size: 'medium',
+    color: 'rgba(123,255,51,0.8)',
   };
 
   constructor(private spinner: NgxSpinnerService) { }
 
-  showSpinner() {
-    this.spinner.show();
-    setTimeout(() => {
-      this.spinner.hide();
-    }, 3000);
+  showSpinner(name?: string) {
+    this.spinner.show(name);
+    setTimeout(() => this.spinner.hide(name), 5000);
   }
+
+  changeAndShowSpinner(){
+    this.loadingText = 'added text';
+    this.spinner.show(undefined, { type: 'line-scale-party', size: 'large', bdColor: 'rgba(100,149,237, .8)', color: 'white'});
+    setTimeout(() => this.spinner.hide(), 3000);
+  }
+
+  goCrazy(){
+    this.spinner.show('1');
+    setInterval(() => this.spinner.show('1'), 1000);
+    setInterval(() => this.spinner.show('2'), 2000);
+    setInterval(() => this.spinner.show('Wasabi'), 3000);
+    setInterval(() => this.spinner.show('Catchup'), 1000);
+    setInterval(() => this.spinner.show(), 5000);
+  }
+
 }
