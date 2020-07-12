b75954f6e6abe350c818f269767ec682e6759bed
Yuvraj Chauhan <yuvraj.chauhan@rishabhsoft.com>
2020-07-12 20:59:33 +0530

    Added pipe for sanitization.

--- projects/ngx-spinner/src/lib/ngx-spinner.module.ts
+++ projects/ngx-spinner/src/lib/ngx-spinner.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxSpinnerComponent } from './ngx-spinner.component';
+import { SafeHtmlPipe } from './safe-html.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NgxSpinnerComponent],
+  declarations: [NgxSpinnerComponent, SafeHtmlPipe],
   exports: [NgxSpinnerComponent]
 })
 export class NgxSpinnerModule { }
