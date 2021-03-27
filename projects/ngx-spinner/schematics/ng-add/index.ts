3315ec0cbb8635cf82fa15343710d8cc0c1831a6
Yuvraj Chauhan <yuvrajchauhan3113@gmail.com>
2021-03-28 01:49:33 +0530

    Disabled schematics for some time.

--- projects/ngx-spinner/schematics/ng-add/index.ts
+++ projects/ngx-spinner/schematics/ng-add/index.ts
@@ -1,12 +1,12 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schematics/tasks';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 // Just return the tree
-export default function (options: any): Rule {
-  return (_tree: Tree, context: SchematicContext) => {
-    const installTaskId = context.addTask(new NodePackageInstallTask());
-    context.addTask(new RunSchematicTask('ng-add-setup-project', options), [installTaskId]);
-    // _context.addTask(new NodePackageInstallTask());
-    // return tree;
+export default function (_options: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    // const installTaskId = context.addTask(new NodePackageInstallTask());
+    // context.addTask(new RunSchematicTask('ng-add-setup-project', options), [installTaskId]);
+    context.addTask(new NodePackageInstallTask());
+    return tree;
   };
 }
