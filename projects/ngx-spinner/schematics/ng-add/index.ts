39a22c02469d1b7edc273a25afa0ea733dd1c261
Yuvraj Chauhan <yuvrajchauhan3113@gmail.com>
2025-02-16 20:42:27 +0530

    Multiple enhancements.

--- projects/ngx-spinner/schematics/ng-add/index.ts
+++ projects/ngx-spinner/schematics/ng-add/index.ts
@@ -1,12 +1,11 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schematics/tasks';
 
 // Just return the tree
 export default function (_options: any): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    // const installTaskId = context.addTask(new NodePackageInstallTask());
-    // context.addTask(new RunSchematicTask('ng-add-setup-project', options), [installTaskId]);
-    context.addTask(new NodePackageInstallTask());
+    const installTaskId = context.addTask(new NodePackageInstallTask());
+    context.addTask(new RunSchematicTask('ng-add-setup-project', _options), [installTaskId]);
     return tree;
   };
 }
