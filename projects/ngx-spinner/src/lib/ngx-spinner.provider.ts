11de498e189b499009b458d542c3d1667350d2b9
Yuvraj Chauhan <yuvrajchauhan3113@gmail.com>
2025-02-16 19:39:19 +0530

    Updated files.

--- projects/ngx-spinner/src/lib/ngx-spinner.provider.ts
+++ projects/ngx-spinner/src/lib/ngx-spinner.provider.ts
@@ -5,6 +5,21 @@ import {
 } from "@angular/core";
 import { NGX_SPINNER_CONFIG, NgxSpinnerConfig } from "./config";
 
+/**
+ * Provides the configuration for the NgxSpinner.
+ *
+ * @param config - The configuration object for NgxSpinner.
+ * @returns An array of environment providers configured with the given NgxSpinner configuration.
+ * @example
+ * ```ts
+ * import { provideSpinnerConfig } from 'ngx-spinner';
+ *
+ * bootstrap(AppComponent, {
+ *   providers: [
+ *     provideSpinnerConfig({type: 'ball-scale-multiple'}),
+ *   ],
+ * })
+ */
 export const provideSpinnerConfig = (
   config: NgxSpinnerConfig,
 ): EnvironmentProviders => {
